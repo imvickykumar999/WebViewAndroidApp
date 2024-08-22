e5cc785609412fe115e382b99b9c25beb620b0e9
Vicky Kumar <imvickykumar999@gmail.com>
2024-08-22 18:58:50 +0530

    added logo

--- app/src/main/java/com/helloworld/webview/MainActivity.kt
+++ app/src/main/java/com/helloworld/webview/MainActivity.kt
@@ -13,6 +13,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.helloworld.webview.ui.theme.WebViewTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,27 +25,36 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun WebViewScreen(url: String) {
-    // AndroidView is used to integrate traditional Android Views in a Composable.
-    androidx.compose.ui.viewinterop.AndroidView(
-        factory = { context ->
-            WebView(context).apply {
-                webViewClient = WebViewClient() // Ensures links open in the WebView itself
-                settings.javaScriptEnabled = true // Enable JavaScript if needed
-                loadUrl(url)
-            }
-        },
-        modifier = Modifier.fillMaxSize()
-    )
-}
+    override fun onBackPressed() {
+        if (::webView.isInitialized && webView.canGoBack()) {
+            webView.goBack()  // Go back in WebView's history
+        } else {
+            super.onBackPressed()  // Exit the app
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun WebViewScreenPreview() {
-    WebViewTheme {
-        WebViewScreen(url = "https://logforge.pythonanywhere.com")
+    @Composable
+    fun WebViewScreen(url: String) {
+        // AndroidView is used to integrate traditional Android Views in a Composable.
+        androidx.compose.ui.viewinterop.AndroidView(
+            factory = { context ->
+                WebView(context).apply {
+                    webViewClient = WebViewClient() // Ensures links open in the WebView itself
+                    settings.javaScriptEnabled = true // Enable JavaScript if needed
+                    loadUrl(url)
+                    webView = this  // Initialize the WebView reference
+                }
+            },
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun WebViewScreenPreview() {
+        WebViewTheme {
+            WebViewScreen(url = "https://blogforge.pythonanywhere.com")
+        }
     }
 }
